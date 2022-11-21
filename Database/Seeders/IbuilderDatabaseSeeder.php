f42294934f104e11293c9e1b04c68bdd1217204f
Juan Escalante <jcec007@gmail.com>
2022-11-21 08:11:41 -0500

    +Add: ModuleTableSeeder

--- Database/Seeders/IbuilderDatabaseSeeder.php
+++ Database/Seeders/IbuilderDatabaseSeeder.php
@@ -16,6 +16,6 @@ class IbuilderDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(IbuilderModuleTableSeeder::class);
     }
 }
