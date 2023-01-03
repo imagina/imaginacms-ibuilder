0343e03fec40ed9c710f0898caab3b1b5021fbbb
msolano <msolano@imaginacolombia.com>
2023-01-03 09:43:21 -0500

    +Add: client permission

--- Config/permissions.php
+++ Config/permissions.php
@@ -6,6 +6,7 @@ return [
         'index' => 'ibuilder::blocks.list resource',
         'create' => 'ibuilder::blocks.create resource',
         'edit' => 'ibuilder::blocks.edit resource',
+        'client' => 'ibuilder::blocks.client resource',
         'destroy' => 'ibuilder::blocks.destroy resource',
         'restore' => 'ibuilder::blocks.restore resource',
     ],
