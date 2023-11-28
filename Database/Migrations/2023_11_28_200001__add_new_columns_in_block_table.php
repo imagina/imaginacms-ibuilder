37979232f6cf5bcc29fcdd325700bc3a3fb79f72
Nicolas <nmontealegre@imaginacolombia.com>
2023-11-28 17:28:48 -0500

    Added: One-to-many relationship in layout-block

---
+++ Database/Migrations/2023_11_28_200001__add_new_columns_in_block_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddNewColumnsInBlockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+     
+      Schema::table('ibuilder__blocks', function (Blueprint $table) {
+
+        $table->text('grid_position')->nullable()->after('system_name');
+        $table->integer('order')->unsigned()->nullable()->after('system_name');
+        $table->integer('layout_id')->unsigned()->nullable()->after('system_name');
+        $table->foreign('layout_id')->references('id')->on('ibuilder__layouts')->onDelete('cascade');
+
+      });
+      
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+      Schema::table('ibuilder__blocks', function (Blueprint $table) {
+
+        $table->dropForeign(['layout_id']);
+        $table->dropColumn('layout_id');
+
+        $table->dropColumn('grid_position');
+        $table->dropColumn('order');
+
+      });
+
+    }
+}
