6afe65665e140efce7a7472acee75180a78f4b80
msolanogithub <msolano@imaginacolombia.com>
2024-02-05 18:05:11 -0500

    fixed minor bugs

--- Resources/views/frontend/components/container.blade.php
+++ Resources/views/frontend/components/container.blade.php
@@ -2,10 +2,10 @@
   // ... any logic
 @endphp
 
-<div class="container-block">
-  <div class="row no-gutters">
+<div class="container-block test">
+  <div class="row">
     @foreach($children as $block)
-      <div class="col-12 {{ $block["gridPosition"] }}">
+      <div class="{{ $block["gridPosition"] }}">
         <x-ibuilder::block :blockConfig="$block"/>
       </div>
     @endforeach
