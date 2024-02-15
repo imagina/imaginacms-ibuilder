ca8c9fe71d76ecf06009afc84101f97792d2f712
adrianacardoso <acardosof27@gmail.com>
2024-02-15 09:16:44 -0400

    +update: container with background image

--- View/Components/Container.php
+++ View/Components/Container.php
@@ -6,9 +6,17 @@ use Illuminate\View\Component;
 
 class Container extends Component
 {
-  public $view, $id, $children;
+  public $view, $id, $children, $containerBlock, $row, $backgrounds, $backgroundImg,
+         $backgroundGeneral, $styleContainer;
 
-  public function __construct($id = null, $children = [])
+  public function __construct($id = null,
+                              $children = [],
+                              $containerBlock = 'overflow-hidden',
+                              $row = '',
+                              $backgrounds = [],
+                              $backgroundImg = '',
+                              $backgroundGeneral = '',
+                              $styleContainer = null)
   {
     $params = get_defined_vars();//Get all params
     $this->instanceGeneralAttributes($params);//Init
@@ -21,8 +29,31 @@ class Container extends Component
   public function instanceGeneralAttributes($params)
   {
     $this->view = "ibuilder::frontend.components.container";
-    $this->id = $params["id"] ?? uniqid();
+    $this->id = $params["id"] ?? uniqid('container-');
     $this->children = $params["children"];
+    $this->containerBlock = $params["containerBlock"];
+    $this->row = $params["row"];
+    $this->backgrounds = $params["backgrounds"];
+    $this->backgroundGeneral = $params["backgroundGeneral"];
+    $this->styleContainer = $params["styleContainer"];
+    $this->backgroundImg = $this->backgroundImage($params["backgroundImg"]);
+  }
+
+  /**
+    *
+    * @return image
+  */
+  public function backgroundImage($paramsImage)
+  {
+    $image = "";
+    if(!empty($paramsImage)){
+        if (!empty($paramsImage->mimeType) && $paramsImage->mimeType=="image/svg+xml") {
+            $image = $paramsImage->path;
+        } elseif(!empty($paramsImage->extraLargeThumb)) {
+            $image = $paramsImage->extraLargeThumb;
+        }
+    }
+    return $image;
   }
 
   /**
