6099dd8949994bc97d436365ca8f4fbd0d1dffb6
josuebedoya <josuebedoya348@gmail.com>
2024-09-16 17:17:35 -0500

    +Add : Back-end | gallery title the contentCustom in editor

--- Resources/views/frontend/components/content-custom/partials/gallery.blade.php
+++ Resources/views/frontend/components/content-custom/partials/gallery.blade.php
@@ -1,16 +1,25 @@
 @if(!empty($item->mediaFiles()->gallery))
-@php
-    $navText = [ "<i class='$galleryNavIcons[0]'></i>", "<i class='$galleryNavIcons[1]'></i>" ];
-@endphp
-<div class="{{$orderClasses["gallery"] ?? 'col-12 order-3'}} custom-item-gallery">
-    <div class="gallery {{$galleryClass}}">
-        <x-media::gallery
-                :layout="$galleryLayout"
-                :responsive="$galleryResponsive"
-                :dots="$galleryDots"
-                :nav="$galleryNav"
-                :navText="$navText"
-                :mediaFiles="$item->mediaFiles()" />
+    @php
+        $navText = [ "<i class='$galleryNavIcons[0]'></i>", "<i class='$galleryNavIcons[1]'></i>" ];
+    @endphp
+    <div class="{{$orderClasses["gallery"] ?? 'col-12 order-3'}} custom-item-gallery">
+        @if($galleryTitleDisplay === "1")
+            <div class="title-gallery col-md-12">
+                <div class="{{$galleryTitleColorByClass}} {{$galleryTitlePosition}}">
+                    <h2 class="title {{$galleryTitleClass}}">{{$galleryTitle}}</h2>
+                </div>
+            </div>
+        @endif
+        <div class="gallery {{$galleryClass}}">
+            <x-media::gallery
+                    :layout="$galleryLayout"
+                    :responsive="$galleryResponsive"
+                    :dots="$galleryDots"
+                    :nav="$galleryNav"
+                    :navText="$navText"
+                    :mediaFiles="$item->mediaFiles()"
+                    :margin="$galleryMarginItems"
+            />
+        </div>
     </div>
-</div>
 @endif
