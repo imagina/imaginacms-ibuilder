7ca5b3c82339e50a5bf22dfa6f47812e352793b2
TJULIAN-D <juliandavidbonillatorres@gmail.com>
2024-10-10 18:08:24 -0500

    + Fix : Back-end | validate change zone  image

--- Resources/views/frontend/components/content-custom/partials/media.blade.php
+++ Resources/views/frontend/components/content-custom/partials/media.blade.php
@@ -1,11 +1,12 @@
-@if(!is_null($item->mediaFiles()) && !is_null($item->mediaFiles()->mainimage->id))
+@php($imageZone = $imageZone ?? 'mainimage')
+@if(!is_null($item->mediaFiles()) && !is_null($item->mediaFiles()->$imageZone->id))
 <div class="{{$orderClasses["media"] ?? 'col-12 order-1'}} custom-item-image">
     <div class="image @if($item->mediaFiles()->mainimage->mediaType=='video') {{$imageClass}} @endif">
         <x-media::single-image
                 :title="$item->title ?? $item->name ?? ''"
                 :isMedia="true"
                 width="100%"
-                :zone="$imageZone ?? 'mainimage'"
+                :zone="$imageZone"
                 :withVideoControls="$videoControls" :loopVideo="$videoLoop"
                 :autoplayVideo="$videoAutoplay" :mutedVideo="$videoMuted"
                 :mediaFiles="$item->mediaFiles() ?? null"
