57b730604cf8c02f965646489c444ce183a33a78
Cristian Marin <marin101998@gmail.com>
2025-03-04 10:38:11 -0500

    + fix | v10 04-03-25 revisions enable in entity block

--- Entities/Block.php
+++ Entities/Block.php
@@ -10,48 +10,54 @@ use Modules\Media\Support\Traits\MediaRelation;
 
 class Block extends CrudModel
 {
-    public $forceDeleting = true;
+  public $forceDeleting = true;
 
-    use Translatable, MediaRelation, isFillable;
+  use Translatable, MediaRelation, isFillable;
 
-    protected $table = 'ibuilder__blocks';
+  protected $table = 'ibuilder__blocks';
 
-    public $transformer = 'Modules\Ibuilder\Transformers\BlockTransformer';
+  public $transformer = 'Modules\Ibuilder\Transformers\BlockTransformer';
 
-    public $repository = 'Modules\Ibuilder\Repositories\BlockRepository';
+  public $repository = 'Modules\Ibuilder\Repositories\BlockRepository';
 
-    public $requestValidation = [
-        'create' => 'Modules\Ibuilder\Http\Requests\CreateBlockRequest',
-        'update' => 'Modules\Ibuilder\Http\Requests\UpdateBlockRequest',
-    ];
+  public $requestValidation = [
+    'create' => 'Modules\Ibuilder\Http\Requests\CreateBlockRequest',
+    'update' => 'Modules\Ibuilder\Http\Requests\UpdateBlockRequest',
+  ];
 
-    //Instance external/internal events to dispatch with extraData
-    public $dispatchesEventsWithBindings = [
-        //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
-        'created' => [],
-        'creating' => [],
-        'updated' => [],
-        'updating' => [],
-        'deleting' => [],
-        'deleted' => [],
-    ];
+  //Instance external/internal events to dispatch with extraData
+  public $dispatchesEventsWithBindings = [
+    //eg. ['path' => 'path/module/event', 'extraData' => [/*...optional*/]]
+    'created' => [],
+    'creating' => [],
+    'updated' => [],
+    'updating' => [],
+    'deleting' => [],
+    'deleted' => [],
+  ];
 
-    public $translatedAttributes = ['internal_title'];
+  public $translatedAttributes = ['internal_title'];
 
-    protected $fillable = ['system_name', 'status', 'component', 'entity', 'attributes'];
+  protected $fillable = ['system_name', 'status', 'component', 'entity', 'attributes'];
 
-    protected $casts = ['component' => 'array', 'entity' => 'array', 'attributes' => AsArrayObject::class];
+  protected $casts = ['component' => 'array', 'entity' => 'array', 'attributes' => AsArrayObject::class];
 
-    public $modelRelations = ['layouts' => 'belongsToMany'];
+  public $modelRelations = ['layouts' => 'belongsToMany'];
 
-    protected $with = ['fields', 'files', 'translations'];
+  protected $with = ['fields', 'files', 'translations'];
 
-    public function layouts()
-    {
-      return $this->belongsToMany(Layout::class, 'ibuilder__layout_blocks');
-    }
-    public function getRenderData()
-    {
-      return mapBlockToRender(blocksToArray([$this])[0], false);
-    }
+  protected $revisionEnabled = true;
+  protected $revisionCleanup = true;
+  protected $historyLimit = 100;
+  protected $revisionCreationsEnabled = true;
+
+  public function layouts()
+  {
+    return $this->belongsToMany(Layout::class, 'ibuilder__layout_blocks');
+  }
+
+  public function getRenderData()
+  {
+    return mapBlockToRender(blocksToArray([$this])[0], false);
+  }
 }
